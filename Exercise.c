8bf8597d07624d85b9649ad24b6b5e0174f24b95
ChethiL <40224396+ChethiL@users.noreply.github.com>
2018-06-18 16:03:48 +0530

    First Commit

---
+++ Exercise.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+ printf("Univercity of Moratuwa\n")
+ printf("Katubedda,\n")
+ printf("Moratuwa,\n")
+ printf("Sri Lanka\n")
+ printf("----------------------\n")
+ printf("www.mrt.ac.lk")
+
+}
